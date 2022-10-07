83566dc653406bba4ca0622c4a5e29b4adbc6701
Elsie Liu <44015504+AltheaL55@users.noreply.github.com>
2022-10-07 17:47:42 -0400

    Finishing Task5

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -29,6 +29,9 @@ line_list = file_object.readlines()
 #Close the file
 file_object.close()
 
+date_dict = {}
+location_dict = {}
+
 #Pretend we read one line of data from the file
 for lineString in line_list:
     if lineString[0]  in ('#','u'):
@@ -36,13 +39,14 @@ for lineString in line_list:
     
     #Split the string into a list of data items
     lineData = lineString.split("\t")
-    
     #Extract items in list into variables
     record_id = lineData[0]
-    obs_date = lineData[2]
+    obs_date = lineData[2][:8]
     obs_lc = lineData[4]
-    obs_lat = lineData[6]
-    obs_lon = lineData[7]
+    obs_lat = lineData[5]
+    obs_lon = lineData[6]
     
     #Print the location of sara
     print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    date_dict[record_id]= obs_date
+    location_dict[record_id]=(obs_lat,obs_lon)
