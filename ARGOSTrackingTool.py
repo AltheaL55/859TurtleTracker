9ac29f9f4fc6b0bb7b0a3f43f8f8de4fef0c0701
Elsie Liu <44015504+AltheaL55@users.noreply.github.com>
2022-10-07 16:27:35 -0400

    create py script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  7 16:25:31 2022
+
+@author: sl691
+"""
+
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Elsie Liu (elsieliu55@gmail.com)
+# Date:   Fall 2022
+#--------------------------------------------------------------
+
