77ce5bfed69b40aa56137ef02b9275570f030158
Elsie Liu <44015504+AltheaL55@users.noreply.github.com>
2022-10-07 18:17:47 -0400

    Finishing Task 7

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -16,6 +16,8 @@ Created on Fri Oct  7 16:25:31 2022
 # Date:   Fall 2022
 #--------------------------------------------------------------
 
+# Ask the user for a date, specifying the format
+user_date = input("Enter a date (M/D/YYYY)")
 
 #Create a variable pointing to the data file
 file_name = './data/raw/sara.txt'
@@ -41,7 +43,7 @@ for lineString in line_list:
     lineData = lineString.split("\t")
     #Extract items in list into variables
     record_id = lineData[0]
-    obs_date = lineData[2][:8]
+    obs_date = lineData[2].split(' ')[0]
     obs_lc = lineData[3]
     obs_lat = lineData[5]
     obs_lon = lineData[6]
@@ -50,6 +52,18 @@ for lineString in line_list:
     else:
         continue
     #Print the location of sara
-    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    #print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
     date_dict[record_id]= obs_date
     location_dict[record_id]=(obs_lat,obs_lon)
+    #Create an empty key list
+    matching_keys = []
+    
+    # Loop through all key, value pairs in the date_dictionary
+    for the_key, the_value in date_dict.items():
+        #See if the date (the value) matches the user date
+        if the_value == user_date:
+            matching_keys.append(the_key)
+#Reveal locations for each key in matching_keys
+for matching_key in matching_keys:
+    obs_lat, obs_lon = location_dict[matching_key]
+    print(f"Record {matching_key} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {user_date}")
