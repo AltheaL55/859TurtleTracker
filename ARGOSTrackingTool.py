fba994aead22ea471172850075b251274eff95d9
Elsie Liu <44015504+AltheaL55@users.noreply.github.com>
2022-10-07 17:34:56 -0400

    Finishing task 4

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -16,18 +16,33 @@ Created on Fri Oct  7 16:25:31 2022
 # Date:   Fall 2022
 #--------------------------------------------------------------
 
-# Copy and paste a line of data as the lineString variable value
-lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
 
-# Use the split command to parse the items in lineString into a list object
-lineData = lineString.split("\t")
+#Create a variable pointing to the data file
+file_name = './data/raw/sara.txt'
 
-# Assign variables to specfic items in the list
-record_id = lineData[0]   # ARGOS tracking record ID
-obs_date = lineData[2]   # Observation date
-ob_lc = lineData[3]       # Observation Location Class
-obs_lat = lineData[5]     # Observation Latitude
-obs_lon = lineData[6]     # Observation Longitude
+#Create a file object from the file
+file_object = open(file_name,'r')
 
-# Print information to the use
-print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
+#Read contents of file into a list
+line_list = file_object.readlines()
+
+#Close the file
+file_object.close()
+
+#Pretend we read one line of data from the file
+for lineString in line_list:
+    if lineString[0]  in ('#','u'):
+        continue
+    
+    #Split the string into a list of data items
+    lineData = lineString.split("\t")
+    
+    #Extract items in list into variables
+    record_id = lineData[0]
+    obs_date = lineData[2]
+    obs_lc = lineData[4]
+    obs_lat = lineData[6]
+    obs_lon = lineData[7]
+    
+    #Print the location of sara
+    print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
